02f1fab8e71f54f86d0a602e7dc3b706414194f1
Ajay Kumar <116066192+ajay-2344@users.noreply.github.com>
2024-04-06 07:32:43 +0000

    new file

---
+++ o1_basics/o1_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144343;
+let accountEmail = "jamesajay@gmai.com";
+var accountPassword = "12433";
+var accountCity = "Bhopal";
+//accountId = 2 not allow
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
